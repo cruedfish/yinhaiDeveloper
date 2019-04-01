5219fc85dbaa9a653b4cb91cd9c6def0b621a113
yinhai <1239128278@qq.com>
2019-04-01 21:51:19 +0800

    提交

--- redis/src/main/java/com/yinhai/developer/redis/LettuceUse.java
+++ redis/src/main/java/com/yinhai/developer/redis/LettuceUse.java
@@ -1,19 +1,21 @@
 package com.yinhai.developer.redis;
 
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.reactive.RedisReactiveCommands;
 import io.lettuce.core.api.reactive.RedisStringReactiveCommands;
+import io.lettuce.core.api.sync.RedisCommands;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
 public class LettuceUse {
   public static  void  main(String[]args) throws Exception{
-      RedisClient redisClient = RedisClient.create("redis://yinhai@39.107.90.7:6379/0");
-      StatefulRedisConnection<String,String> statefulRedisConnection = redisClient.connect();
-      RedisStringReactiveCommands<String,String> commands = statefulRedisConnection.reactive();
 //      RedisCommands redisCommand = statefulRedisConnection.sync();
 //      redisCommand.set("key","HelloRedis");
 //      String result = (String)redisCommand.get("key");
@@ -59,36 +61,80 @@ public class LettuceUse {
 //              .flatMap(commands::get)
 //              .flatMap(value -> ((RedisReactiveCommands<String, String>) commands).rpush("result", value))
 //              .subscribe();
-      Flux.just("Ben", "Michael", "Mark").doOnNext(new Consumer<String>() {
-                                                       @Override
-                                                       public void accept(String s) {
-                                                           ((RedisReactiveCommands) commands).sadd(s, "test1");
-                                                       }
-                                                   }).subscribe();
+//      RedisClient redisClient = RedisClient.create("redis://yinhai@39.107.90.7:6379/0");
+////      StatefulRedisConnection<String,String> statefulRedisConnection = redisClient.connect();
+////      RedisCommands<String,String> redisCommands = statefulRedisConnection.sync();
+////      System.out.println(redisCommands.get("key"));
+////      RedisStringReactiveCommands<String,String> commands = statefulRedisConnection.reactive();
+////      Mono<String> mono = commands.get("key");
+////      int a = 1;
+      RedisURI redisURI = RedisURI.Builder.redis("39.107.90.7").withPort(6379).withPassword("yinhai").withDatabase(0).build();
+      RedisClient client = RedisClient.create(redisURI);
+      RedisStringReactiveCommands<String, String> commands = client.connect().reactive();
+      commands.get("key").subscribe(new Consumer<String>() {
+          @Override
+          public void accept(String s) {
+              System.out.println(s);
+          }
+      });
+//      commands
+//              .get("key")
+//              .subscribe(new Subscriber<String>() {
+//                  @Override
+//                  public void onSubscribe(Subscription s) {
+//                      System.out.println("订阅"+s.toString());
+//                  }
+//
+//                  @Override
+//                  public void onNext(String s) {
+//                      System.out.println("next"+s);
+//                  }
+//
+//                  @Override
+//                  public void onError(Throwable t) {
+//
+//                  }
+//
+//                  @Override
+//                  public void onComplete() {
+//                      System.out.println("完成");
+//                  }
+//              });
+
+      Flux.just("Ben", "key", "Mark").flatMap(key -> commands.get(key)).subscribe(value -> System.out.println("Got value: " + value));
 
 
-      Flux.just("Ben", "Michael", "Mark")
-              .doOnNext(((RedisReactiveCommands<String,String>) commands)::scard)
-              .reduce((sum, current) -> sum + current)
-              .subscribe(result -> System.out.println("Number of elements in sets: " +
-                      result));
+//      Flux.just("Ben", "Michael", "Mark").doOnNext(new Consumer<String>() {
+//                                                       @Override
+//                                                       public void accept(String s) {
+//                                                           System.out.println("测试"+s);
+//
+//                                                       }
+//                                                   }).subscribe();
 
 
-      Flux.just("Ben", "Michael", "Mark")
-              .flatMap(((RedisReactiveCommands<String, String>) commands)::scard)
-              .reduce(new BiFunction<Long, Long, Long>() {
-                  @Override
-                  public Long apply(Long aLong, Long aLong2) {
-                      return aLong+aLong2;
-                  }
-              })
-              .subscribe(new Consumer<Long>() {
-                  @Override
-                  public void accept(Long aLong) {
-                      System.out.println("Number of elements in sets: " +
-                              aLong);
-                  }
-              } );
+//      Flux.just("Ben", "Michael", "Mark")
+//              .doOnNext(((RedisReactiveCommands<String,String>) commands)::scard)
+//              .reduce((sum, current) -> sum + current)
+//              .subscribe(result -> System.out.println("Number of elements in sets: " +
+//                      result));
+
+
+//      Flux.just("Ben", "Michael", "Mark")
+//              .flatMap(((RedisReactiveCommands<String, String>) commands)::scard)
+//              .reduce(new BiFunction<Long, Long, Long>() {
+//                  @Override
+//                  public Long apply(Long aLong, Long aLong2) {
+//                      return aLong+aLong2;
+//                  }
+//              })
+//              .subscribe(new Consumer<Long>() {
+//                  @Override
+//                  public void accept(Long aLong) {
+//                      System.out.println("Number of elements in sets: " +
+//                              aLong);
+//                  }
+//              } );
 
   }
 }
